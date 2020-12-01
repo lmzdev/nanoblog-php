2f4f54c347778ab85214621cb46138a2118b9443
Menzel <dev@leomenzel.de>
2020-12-01 13:22:30 +0100

    initial github commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # nanoblog-php
-Simple nanoblogging boilerplate based on PHP and SQLite
+PHP Boilerplate for storing Text/Timestamp entries in a SQLite Database.
+
+### Introduction 
+I needed a dead-simple solution for storing status updates without any additional setup. Also, my website resides on a shared hosting plan where only PHP is available.
+
+The solutions i found where either too static or too bloated to my liking, so i came up with my own. But beware: As of now i guess this Project only qualifies as a collection of code snippets or a boilerplate at best.
+
+If you are looking for a more refined and ready-to-use microblogging solution, take a look at [oelna/microblog](https://github.com/oelna/microblog). 
+
+### Usage
+1. Copy ```nanoblog.php``` to your webspace.
+1. Include with ``` <?php include('nanoblog.php') ?>``` or only use via HTTP Requests
+#### Add Entry
+* Use ```db_insert``` function
+* or do a HTTP POST Request and include _add_ and _secret_ , eg. ```add=<newconent>&secret=<mysecret>```
+
+#### Remove Entry
+* Remove latest entry with ```db_delete_latest``` function
+* or do a HTTP POST Request and include _delete_ and _secret_ , eg. ```delete&secret=<mysecret>```
+
+### Hints
+
+
+I included ```nanoblog.php``` into my ```index.php``` and did the following to display a list of Posts:
+```php
+<?php
+$posts = db_select_posts();
+if ($posts) {
+    foreach ($posts as $item) {
+        $datetime = strftime('%a., %d.%m.%Y - %H:%M', $item['post_timestamp']);
+        $post = $item["post_content"]; ?>
+        <article class="post">
+            <b class="date"><?= $datetime ?></b><br>
+                <?= $post ?>
+        </article>
+<?php
+    }
+}
+>
+```
+
+Insertion and removal is done through a JavaScript-Client which hits the HTTP Endpoints with ```XMLHttpRequest```
---
+++ nanoblog.php
@@ -0,0 +1,64 @@
+<?php
+DEFINE('ROOT', __DIR__);
+DEFINE('DS', DIRECTORY_SEPARATOR);
+DEFINE('NOW', time());
+DEFINE('SECRET', 'chewbacca');
+
+// setlocale(LC_TIME, "de_DE");
+// date_default_timezone_set('Europe/Berlin');
+
+try {
+	$db = new PDO('sqlite:'.ROOT.DS.'posts.db');
+	$db->exec("CREATE TABLE IF NOT EXISTS posts (
+		id integer PRIMARY KEY NOT NULL,
+		post_content TEXT,
+		post_timestamp integer
+	);");
+} catch(PDOException $e) {
+	print 'Exception : '.$e->getMessage();
+	die('cannot connect to or open the database');
+}
+
+function db_insert($message, $timestamp=NOW) {
+	global $db;
+	if (strlen($message)===0) return;
+	if(empty($db)) return false;
+	$statement = $db->prepare('INSERT INTO posts (post_content, post_timestamp) VALUES (:post_content, :post_timestamp)');
+	$statement->bindParam(':post_content', $message, PDO::PARAM_STR);
+	$statement->bindParam(':post_timestamp', $timestamp, PDO::PARAM_INT);
+	$statement->execute();
+	return $db->lastInsertId();
+}
+
+function db_delete_latest() {
+	global $db;
+	if(empty($db)) return false;
+	$statement = $db->prepare('DELETE FROM posts ORDER BY id DESC LIMIT 1');
+	$statement->execute();
+	return true;
+}
+
+function db_select_posts($from=NOW, $amount=10, $sort='desc', $page=1) {
+	global $db;
+	if(empty($db)) return false;
+	if($sort !== 'desc') $sort = 'asc';
+    $postsperpage = 20;
+	$offset = ($page-1)*$postsperpage;
+	$statement = $db->prepare('SELECT * FROM posts WHERE post_timestamp < :post_timestamp ORDER BY id '.$sort.' LIMIT :limit OFFSET :page');
+	$statement->bindParam(':post_timestamp', $from, PDO::PARAM_INT);
+	$statement->bindParam(':limit', $amount, PDO::PARAM_INT);
+	$statement->bindParam(':page', $offset, PDO::PARAM_INT);
+	$statement->execute();
+	$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+	return (!empty($rows)) ? $rows : false;
+}
+
+
+
+// HTTP API
+
+if (isset($_POST["add"]) ) {
+    print $_POST["secret"] === SECRET?db_insert($_POST["add"]):"access denied";
+} elseif (isset($_POST["delete"]) ) {
+    print $_POST["secret"] === SECRET? db_delete_latest():"access denied";
+}
